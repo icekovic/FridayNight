4306aeb82ee0aa8b9e5504155924eac60b30c69f
icekovic <icekovic@racunarstvo.hr>
2019-01-11 10:15:02 +0100

    added connection to the Startup.cs

--- FridayNight/Startup.cs
+++ FridayNight/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FridayNight.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,11 @@ namespace FridayNight
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=FridayNight;UserID=sa;Password=!v@n";
+            services.AddDbContext<FridayNightContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
